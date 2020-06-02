699daa0eafba9980f53a7fcf26731d0f107d35f4
Gabriel Asorey <gasorey@gmail.com>
2020-06-02 14:26:45 -0300

    nlw

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
 import express from 'express'
-
+import routes from './routes'
+import path from 'path'
+import cors from 'cors'
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('listagem de usuários')
+app.use(cors())
+
+app.use(express.json())
+app.use(routes)
+app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')))
 
 
-  response.json({
-    message: 'hello world'
-  })
-})
+
+
 
 app.listen(3333)
