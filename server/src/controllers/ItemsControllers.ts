d7e7ea0b958e648600d2bb224df457fbcd60bd75
Gabriel Asorey <gasorey@gmail.com>
2020-06-05 16:21:23 -0300

    nlw

--- server/src/controllers/ItemsControllers.ts
+++ server/src/controllers/ItemsControllers.ts
@@ -3,20 +3,19 @@ import {Request, Response} from 'express'
 import knex from '../database/connection'
 
 class ItemsControllers {
-  async index(request: Request, response: Response){
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
 
-      const items = await  knex('items').select('*')  
-    
-      const serializaedItems = items.map(item => {
-        return {
-          id: item.id,
-          title: item.title,
-          image_url: `http://localhost:3333/uploads/${item.image}`
-        }
-      })
-      
-      response.json(serializaedItems)
-    }
+    const serializaedItems = items.map(item => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://localhost:3333/uploads/${item.image}`,
+      };
+    });
+
+    response.json(serializaedItems);
+  }
   }
 
   export default ItemsControllers
