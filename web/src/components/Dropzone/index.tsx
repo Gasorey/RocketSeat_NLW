852de40c2d7dc77d0d61e420775436a35909977d
Gabriel Asorey <gasorey@gmail.com>
2020-06-07 00:21:22 -0300

    nlw

--- web/src/components/Dropzone/index.tsx
+++ web/src/components/Dropzone/index.tsx
@@ -1,1 +1,46 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import './styles.css'
+import {FiUpload} from 'react-icons/fi'
+
+
+interface Props {
+  onFileUploaded: (file: File) => void
+}
+
+const Dropzone: React.FC<Props> = ({onFileUploaded}) => {
+
+  const [selectedFileURL, setSelectedFileURL] = useState('')
+
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+
+    const fileURL = URL.createObjectURL(file)
+    setSelectedFileURL(fileURL)
+    onFileUploaded(file)
+  }, [onFileUploaded])
+  const { getRootProps, getInputProps} = useDropzone({
+    onDrop,
+    accept:'image/*'
+    
+  })
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      
+      <input {...getInputProps()} accept="image/*"/>
+
+     {
+       selectedFileURL 
+       ? <img src={selectedFileURL} alt ="thumb" />
+       : (
+          <p>
+        <FiUpload/>
+        Por favor insira a imagem do estabelecimento
+        </p>
+        )
+     }
+    </div>
+ )
+}
+export default Dropzone
